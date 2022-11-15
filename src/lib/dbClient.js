6a791ef6979e06a19a8fab2e1fd2a3aaa439ba19
Alan Keown <bcycadata@outlook.com>
2022-11-16 07:58:44 +1100

    Maybe

--- src/lib/dbClient.js
+++ src/lib/dbClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 import { env } from '$env/dynamic/public';
 
-export const supabaseRedirectBase = env.VITE_SUPABASE_REDIRECT_URL_BASE;
+export const supabaseRedirectBase = env.PUBLIC_SUPABASE_REDIRECT_URL_BASE;
 
 export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
