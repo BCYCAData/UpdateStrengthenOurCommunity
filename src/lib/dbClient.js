dad64cb3b8b98834f1043672e93d2693d28d06fc
Alan Keown <bcycadata@outlook.com>
2022-11-16 08:09:39 +1100

    Update dbClient.js

--- src/lib/dbClient.js
+++ src/lib/dbClient.js
@@ -3,4 +3,6 @@ import { env } from '$env/dynamic/public';
 
 export const supabaseRedirectBase = env.PUBLIC_SUPABASE_REDIRECT_URL_BASE;
 
+console.log('env', env);
+
 export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
